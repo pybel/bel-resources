2dea25971e0da8e283f5ce5d253c07066ab7becc
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-02-24 21:12:39 +0100

    Add GitHub utilities

---
+++ src/bel_resources/github.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Generate summary pages of BEL graphs in HTML."""
+
+import sys
+
+import requests
+
+FILE_API_URL = 'https://api.github.com/repos/{owner}/{repo}/commits?path={path}'
+RAW_URL = 'https://raw.githubusercontent.com/{owner}/{repo}/{sha}/{path}'
+
+
+def get_github_hash(owner: str, repo: str, path: str) -> str:
+    """Get the SHA hash corresponding to the most recent update to the BEL namespace file on a GitHub repository."""
+    url = FILE_API_URL.format(owner=owner, repo=repo, path=path.lstrip('/'))
+    res = requests.get(url)
+    res_json = res.json()
+    most_recent_commit = res_json[0]
+    return most_recent_commit['sha']
+
+
+def get_github_url(owner: str, repo: str, path: str) -> str:
+    """Get the URL corresponding to the most recent update to the BEL namespace file on a GitHub repository."""
+    sha = get_github_hash(owner, repo, path)
+    return RAW_URL.format(owner=owner, repo=repo, sha=sha, path=path.lstrip('/'))
+
+
+def get_conso_names_url() -> str:
+    """Get the URL for the most recent version of Curation of Neurodegeneration Supporting Ontology (CONSO) names."""
+    return get_github_url('pharmacome', 'terminology', 'export/hbp-names.belns')
+
+
+def get_conso_identifiers_url() -> str:
+    """Get the URL for the most recent version of CONSO identifiers."""
+    return get_github_url('pharmacome', 'terminology', 'export/hbp.belns')
+
+
+def get_famplex_url() -> str:
+    """Get the URL for the most recent version of FamPlex names."""
+    return get_github_url('sorgerlab', 'famplex', 'export/famplex.belns')
+
+
+if __name__ == '__main__':
+    print(get_github_hash(*sys.argv[1:4]))
